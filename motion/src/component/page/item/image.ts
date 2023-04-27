21db4f2707fa8d274b4e5fd6dbc712b3f2761f4d
tintoll <belselios@gmail.com>
2023-04-28 05:55:40 +0900

    리팩토링 - 중복되는 부분 클래스로 추출

--- motion/src/component/page/item/image.ts
+++ motion/src/component/page/item/image.ts
@@ -1,15 +1,11 @@
-export class ImageComponent {
-  private element: HTMLElement;
-  constructor(title: string, url: string) {
-    const template = document.createElement('template');
-    template.innerHTML = `<section class="image">
-    <div class="image__holder"><img class="image__thumbnail" /></div>
-    <p class="image__title"></p>
-  </section>`;
-    // 전달받은 title, url을 바로 innerHTML로 전달하는 것은 위험하다.
-    // 필요한 요소안에 접근하여 값을 설정해줘야 한다.
+import { BaseComponent } from '../../component.js';
 
-    this.element = template.content.firstElementChild! as HTMLElement;
+export class ImageComponent extends BaseComponent<HTMLElement> {
+  constructor(title: string, url: string) {
+    super(`<section class="image">
+            <div class="image__holder"><img class="image__thumbnail" /></div>
+            <p class="image__title"></p>
+           </section>`);
 
     const imageElement = this.element.querySelector(
       '.image__thumbnail'
@@ -21,9 +17,5 @@ export class ImageComponent {
       '.image__title'
     )! as HTMLParagraphElement;
     titleElement.textContent = title;
-  }
-
-  attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
-    parent.insertAdjacentElement(position, this.element);
   }
 }
